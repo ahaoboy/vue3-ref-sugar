06dd3ff88ded6e0c10b939067a650724e0bd60d4
严育豪 <yanyuhao.ace@bytedance.com>
2020-11-08 21:13:33 +0800

    init

---
+++ src/App copy.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { computed, reactive, watch } from "vue";
+
+ref: count = 0;
+ref: doubled = computed(() => count * 2);
+
+const inc = () => count++;
+const dec = () => count--;
+
+const r = reactive({ count: 0 });
+watch(() => {
+  r.count = count;
+  r.doubled = 2 * count;
+});
+
+console.log(new Date().toLocaleString());
+// await new Promise((r) => setTimeout(r, 1000));
+console.log(new Date().toLocaleString());
+</script>
+
+<template>
+  <div>{{ count }} * 2 = {{ doubled }}</div>
+  <div>{{ r.count }} * 2 = {{ r.doubled }}</div>
+  <button @click="dec">Dec</button>
+  <button @click="inc">Inc</button>
+</template>
---
+++ src/Count.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>count</div>
+</template>
+
+<script setup>
+console.log(new Date().toLocaleString());
+await new Promise((r) => setTimeout(r, 1000));
+console.log(new Date().toLocaleString());
+</script>
+
+<style></style>
