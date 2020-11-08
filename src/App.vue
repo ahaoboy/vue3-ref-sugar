284e0e859238de188ee4a17804f12b509b92e1c7
严育豪 <yanyuhao.ace@bytedance.com>
2020-11-08 21:13:22 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <script setup>
-import { computed } from 'vue'
+import { computed, reactive, watch } from "vue";
+import Count from "./Count.vue";
+ref: count = 0;
+ref: doubled = computed(() => count * 2);
 
-ref: count = 0
-ref: doubled = computed(() => count * 2)
+const inc = () => count++;
+const dec = () => count--;
 
-const inc = () => count++
-const dec = () => count -= 1
+const r = reactive({ count: 0 });
+watch(() => {
+  r.count = count;
+  r.doubled = 2 * count;
+});
+
+console.log(new Date().toLocaleString());
+// await new Promise((r) => setTimeout(r, 1000));
+console.log(new Date().toLocaleString());
 </script>
 
 <template>
   <div>{{ count }} * 2 = {{ doubled }}</div>
+  <div>{{ r.count }} * 2 = {{ r.doubled }}</div>
   <button @click="dec">Dec</button>
   <button @click="inc">Inc</button>
+  <Suspense>
+    <Count />
+  </Suspense>
 </template>
